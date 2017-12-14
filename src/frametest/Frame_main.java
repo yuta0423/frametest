d4ecf6ccccb589e2157b22937e6eb2d3bce781ab
50616032 <50616032@50616032-pc>
2017-12-15 00:57:44 +0900

    50616032

---
+++ src/frametest/Frame_main.java
@@ -0,0 +1,14 @@
+package frametest;
+
+public class Frame_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Frame_view frame = new Frame_view();
+		frame.setBounds(5,5,655,455);
+		frame.setVisible(true);
+		
+		
+	}
+
+}
